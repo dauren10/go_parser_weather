cf4c2924f1fe71b45a388cbb2d590187554f2161
Dauren Shalabayev <Dauren.Shalabayev@kcell.kz>
2020-08-18 16:39:32 +0600

    first commit

---
+++ parser.go
@@ -0,0 +1,97 @@
+// You can edit this code!
+// Click here and start typing.
+package main
+import "net/url"
+import "fmt"
+import "net/http"
+import "log"
+import "io/ioutil"
+import "encoding/xml"
+
+type MMWEATHER struct {
+	XMLName xml.Name `xml:"MMWEATHER"`
+	Text    string   `xml:",chardata"`
+	REPORT  struct {
+		Text string `xml:",chardata"`
+		Type string `xml:"type,attr"`
+		TOWN struct {
+			Text      string `xml:",chardata"`
+			Index     string `xml:"index,attr"`
+			Sname     string `xml:"sname,attr"`
+			Latitude  string `xml:"latitude,attr"`
+			Longitude string `xml:"longitude,attr"`
+			FORECAST  []struct {
+				Text      string `xml:",chardata"`
+				Day       int `xml:"day,attr"`
+				Month     string `xml:"month,attr"`
+				Year      string `xml:"year,attr"`
+				Hour      string `xml:"hour,attr"`
+				Tod       string `xml:"tod,attr"`
+				Predict   string `xml:"predict,attr"`
+				Weekday   string `xml:"weekday,attr"`
+				PHENOMENA struct {
+					Text          string `xml:",chardata"`
+					Cloudiness    string `xml:"cloudiness,attr"`
+					Precipitation string `xml:"precipitation,attr"`
+					Rpower        string `xml:"rpower,attr"`
+					Spower        string `xml:"spower,attr"`
+				} `xml:"PHENOMENA"`
+				PRESSURE struct {
+					Text string `xml:",chardata"`
+					Max  string `xml:"max,attr"`
+					Min  string `xml:"min,attr"`
+				} `xml:"PRESSURE"`
+				TEMPERATURE struct {
+					Text string `xml:",chardata"`
+					Max  string `xml:"max,attr"`
+					Min  string `xml:"min,attr"`
+				} `xml:"TEMPERATURE"`
+				WIND struct {
+					Text      string `xml:",chardata"`
+					Min       string `xml:"min,attr"`
+					Max       string `xml:"max,attr"`
+					Direction string `xml:"direction,attr"`
+				} `xml:"WIND"`
+				RELWET struct {
+					Text string `xml:",chardata"`
+					Max  string `xml:"max,attr"`
+					Min  string `xml:"min,attr"`
+				} `xml:"RELWET"`
+				HEAT struct {
+					Text string `xml:",chardata"`
+					Min  string `xml:"min,attr"`
+					Max  string `xml:"max,attr"`
+				} `xml:"HEAT"`
+			} `xml:"FORECAST"`
+		} `xml:"TOWN"`
+	} `xml:"REPORT"`
+} 
+
+
+func main() {
+	resp, err := http.Get("https://xml.meteoservice.ru/export/gismeteo/point/1.xml")
+	if err!=nil{
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	byteValue, err := ioutil.ReadAll(resp.Body)
+	if err!=nil{
+		log.Fatal(err)
+	}
+	fmt.Println(string(byteValue))
+	var weather MMWEATHER
+	err=xml.Unmarshal(byteValue,&weather)
+
+	town:=weather.REPORT.TOWN.Sname
+	fmt.Println(url.PathUnescape(town))
+	forecast := weather.REPORT.TOWN.FORECAST;
+	for i:=0; i<len(forecast);i++{
+		fmt.Printf("%d/%s %s:00 T.Max: %s\n", 
+			forecast[i].Day,
+			forecast[i].Month,
+			forecast[i].Hour,
+			forecast[i].TEMPERATURE.Max,
+
+		)
+	}
+}
